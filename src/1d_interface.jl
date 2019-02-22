b29c4f1b4fe8fe685409ae9c9c7557465203198b
Felix Henneke <felix.henneke@fu-berlin.de>
2019-02-22 12:07:56 +0100

    moved 1d lanczos methods to 1d file for now
    1d version might be broken now

--- src/1d_interface.jl
+++ src/1d_interface.jl
@@ -553,3 +553,51 @@ function W_times_vector_fast!(y, x, W_tilde_hat, u_v_vv_conj, u_c_cc, u_c_cc_con
 
     return y
 end
+
+function optical_absorption_vector(prob::BSEProblem1D)
+    return optical_absorption_vector(prob.u_v, prob.u_c, prob.E_v, prob.E_c, prob.prob.r_unit, prob.prob.k_bz)
+end
+
+function optical_absorption_vector(u_v, u_c, E_v, E_c, r_unit, k_bz) # TODO: make general? only works in 1d atm
+    N_unit = length(r_unit)
+    N_v = size(u_v, 2)
+    N_c = size(u_c, 2)
+    N_k = length(k_bz)
+    l = N_unit * (r_unit[2] - r_unit[1])
+
+    d = vec([im * dot(u_c[:, ic, ik],
+                  modified_momentum_operator(r_unit, l, k_bz[ik]) *
+                    u_v[:, iv, ik]) /
+            (E_c[ic, ik] - E_v[iv, ik])
+            for iv in 1:N_v, ic in 1:N_c, ik in 1:N_k])
+
+    return d
+end
+
+function optical_absorption(prob::BSEProblem1D, g, Erange)
+    H = assemble_exact_H(prob)
+    F = eigen(Hermitian(H))
+    ev, ef = F.values, F.vectors
+
+    absorption = optical_absorption(ev, ef, prob.u_v, prob.u_c, prob.E_v, prob.E_c, prob.prob.r_unit, prob.prob.k_bz, g, Erange)
+
+    return absorption
+end
+
+function optical_absorption(ev, ef, u_v, u_c, E_v, E_c, r_unit, k_bz, g, Erange)
+    N_v = size(u_v, 2)
+    N_c = size(u_c, 2)
+    N_k = length(k_bz)
+    l = (r_unit[2] - r_unit[1]) * length(r_unit)
+
+    d = optical_absorption_vector(u_v, u_c, E_v, E_c, r_unit, k_bz)
+    osc = [abs2(dot(d, ef[:, n])) for n in 1:(N_v * N_c * N_k)]
+
+    absorption = zeros(length(Erange))
+    for j in 1:(N_v * N_c * N_k)
+        absorption .+= osc[j] * g.(Erange .- ev[j])
+    end
+    absorption .*= 8 * π^2 / l
+
+    return absorption
+end
--- src/lanczos.jl
+++ src/lanczos.jl
@@ -55,26 +55,6 @@ function lanczos_eig(α::AbstractVector, β, N_iter)
     return ev_lanczos, ef_lanczos
 end
 
-function optical_absorption_vector(prob::BSEProblem1D)
-    return optical_absorption_vector(prob.u_v, prob.u_c, prob.E_v, prob.E_c, prob.prob.r_unit, prob.prob.k_bz)
-end
-
-function optical_absorption_vector(u_v, u_c, E_v, E_c, r_unit, k_bz) # TODO: make general? only works in 1d atm
-    N_unit = length(r_unit)
-    N_v = size(u_v, 2)
-    N_c = size(u_c, 2)
-    N_k = length(k_bz)
-    l = N_unit * (r_unit[2] - r_unit[1])
-
-    d = vec([im * dot(u_c[:, ic, ik],
-                  modified_momentum_operator(r_unit, l, k_bz[ik]) *
-                    u_v[:, iv, ik]) /
-            (E_c[ic, ik] - E_v[iv, ik])
-            for iv in 1:N_v, ic in 1:N_c, ik in 1:N_k])
-
-    return d
-end
-
 function lanczos_optical_absorption(prob::AbstractBSEProblem, isdf::ISDF, N_iter, g, Erange) #TODO: adapt to 3d
     l = prob.prob.l
 
@@ -105,32 +85,4 @@ end
 function lanczos_optical_absorption(H, d, N_iter, g, Erange)
     α, β, U = lanczos(H, d, N_iter)
     return lanczos_optical_absorption(α, β, N_iter, g, Erange)
-end
-
-function optical_absorption(prob::BSEProblem1D, g, Erange)
-    H = assemble_exact_H(prob)
-    F = eigen(Hermitian(H))
-    ev, ef = F.values, F.vectors
-
-    absorption = optical_absorption(ev, ef, prob.u_v, prob.u_c, prob.E_v, prob.E_c, prob.prob.r_unit, prob.prob.k_bz, g, Erange)
-
-    return absorption
-end
-
-function optical_absorption(ev, ef, u_v, u_c, E_v, E_c, r_unit, k_bz, g, Erange)
-    N_v = size(u_v, 2)
-    N_c = size(u_c, 2)
-    N_k = length(k_bz)
-    l = (r_unit[2] - r_unit[1]) * length(r_unit)
-
-    d = optical_absorption_vector(u_v, u_c, E_v, E_c, r_unit, k_bz)
-    osc = [abs2(dot(d, ef[:, n])) for n in 1:(N_v * N_c * N_k)]
-
-    absorption = zeros(length(Erange))
-    for j in 1:(N_v * N_c * N_k)
-        absorption .+= osc[j] * g.(Erange .- ev[j])
-    end
-    absorption .*= 8 * π^2 / l
-
-    return absorption
 end
