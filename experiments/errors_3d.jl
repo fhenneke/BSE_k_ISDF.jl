14c65c28ea653e8230fa8984d49598b4e7de1a59
Felix Henneke <felix.henneke@fu-berlin.de>
2019-02-28 15:50:09 +0100

    updated computation of spectra, errors, plotting

--- experiments/errors_3d.jl
+++ experiments/errors_3d.jl
@@ -2,7 +2,7 @@
 
 # %%
 # loading packages
-using BenchmarkTools, JLD2, FileIO, DelimitedFiles, LinearAlgebra, FFTW, Statistics
+using BenchmarkTools, JLD2, FileIO, DelimitedFiles, LinearAlgebra, FFTW, Statistics, Arpack, HDF5
 # using Plots, LaTeXStrings
 # pyplot()
 # theme(:dark)
@@ -18,7 +18,7 @@ using BSE_k_ISDF
 
 # %% set up problem
 
-path = "/home/felix/Work/Research/Code/BSE_k_ISDF/experiments/diamond/131313_20/"
+example_path = "/home/felix/Work/Research/Code/BSE_k_ISDF/experiments/diamond/131313_20/"
 
 N_1d = 20 # TODO: read from file
 N_rs = (N_1d, N_1d, N_1d)
@@ -26,7 +26,7 @@ N_core = 0
 N_v = 4
 N_c = 5 # maybe also use 5 here
 N_ks = (13, 13, 13) # TODO: read from file
-@time prob = BSE_k_ISDF.BSEProblemExciting(N_core, N_v, N_c, N_ks, N_rs, path);
+@time prob = BSE_k_ISDF.BSEProblemExciting(N_core, N_v, N_c, N_ks, N_rs, example_path);
 
 # %% error for different N_μ
 
@@ -72,22 +72,18 @@ end
     push!(errors_M_vc, error_M_vc)
 end
 
-save(path * "/errors_M.jld2", "N_μs_vec", N_μs_vec, "errors_M_vv", errors_M_vv, "errors_M_cc", errors_M_cc, "errors_M_vc", errors_M_vc)
+save(example_path * "/errors_M.jld2", "N_μs_vec", N_μs_vec, "errors_M_vv", errors_M_vv, "errors_M_cc", errors_M_cc, "errors_M_vc", errors_M_vc)
 
 # %% compute spectra for different N_μ
 
 # load erros in M
-N_μs_vec, errors_M_vv, errors_M_cc, errors_M_vc =  load(path * "/errors_M.jld2", "N_μs_vec", "errors_M_vv", "errors_M_cc", "errors_M_vc")
-
-ev = vec([prob.E_c[ic, ik] - prob.E_v[iv, ik] for iv in 1:N_v, ic in 1:N_c, ik in 1:(prod(N_ks))]);
-pmat = BSE_k_ISDF.read_pmat(N_core, N_v, N_c, prod(N_ks), path)
+N_μs_vec, errors_M_vv, errors_M_cc, errors_M_vc =  load(example_path * "/errors_M.jld2", "N_μs_vec", "errors_M_vv", "errors_M_cc", "errors_M_vc")
 
 # fixed parameters
-direction = 1
 N_iter = 200
 
 #variable parameters
-M_tol_vec = [0.5, 0.25, 0.1]
+M_tol_vec = [0.5, 0.25, 0.2]
 
 # compute spectra
 for M_tol in M_tol_vec
@@ -97,56 +93,71 @@ for M_tol in M_tol_vec
 
     isdf = BSE_k_ISDF.ISDF(prob, N_μ_vv, N_μ_cc, N_μ_vc)
 
-    # if M_tol == 0.1
-    #     prob.u_v = 0
-    #     prob.u_c = 0
-    #     GC.gc()
-    # end
-
     H = BSE_k_ISDF.setup_H(prob, isdf)
 
-    d = pmat[:, direction] ./ ev
+    for direction in 1:3
+        d = BSE_k_ISDF.optical_absorption_vector(prob, direction)
 
-    α, β, U = BSE_k_ISDF.lanczos(H, normalize(d), I, N_iter)
+        α, β, U = BSE_k_ISDF.lanczos(H, normalize(d), N_iter)
 
-    # eigenvalues, eigenvectors = eigs(H, which=:SR, nev = 1, maxiter=1000)
+        # save results
+        save(example_path * "/optical_absorption_lanczos_$(M_tol)_$(direction).jld2", "alpha", α, "beta", β, "norm_d", norm(d))
+    end
 
-    # save results
-    save(path * "/optical_absorption_lanczos_$(M_tol).jld2", "alpha", α, "beta", β, "norm d^2", norm(d)^2)
+    eigenvalues, eigenvectors = eigs(H, which=:SR, nev = 1, maxiter=1000)
 
-    # save(path * "/eigs_$(M_tol).jld2", "eigenvalues", eigenvalues, "eigenvectors", eigenvectors)
+    save(example_path * "/eigs_$(M_tol).jld2", "eigenvalues", eigenvalues, "eigenvectors", eigenvectors)
+end
+
+# %% save spectra
+
+# load reference spectrum
+Erange_reference = readdlm(example_path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC11.OUT")[19:end, 1]
+Ha_to_eV = 27.211396641308
+Erange = Erange_reference ./ Ha_to_eV
+
+absorption_reference = zeros(length(Erange_reference), 3)
+absorption_reference[:, 1] = readdlm(example_path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC11.OUT")[19:end, 3]
+absorption_reference[:, 2] = readdlm(example_path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC22.OUT")[19:end, 3]
+absorption_reference[:, 3] = readdlm(example_path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC33.OUT")[19:end, 3]
+
+save(example_path * "/optical_absorption_reference.jld2", "Erange", Erange, "absorption", absorption_reference)
+
+# compute spectra from α and β
+σ = 0.0055
+g = ω -> 1 / π * σ / (ω^2 + σ^2)
+
+for M_tol in M_tol_vec
+    absorption = zeros(length(Erange))
+    for direction in 1:3
+        α, β, norm_d = load(example_path * "/optical_absorption_lanczos_$(M_tol)_$(direction).jld2", "alpha", "beta", "norm_d")
+
+        absorption .+= BSE_k_ISDF.lanczos_optical_absorption(α, β, N_iter, g, Erange, norm_d^2 * 8 * pi^2 / (size(prob)[3] * BSE_k_ISDF.Ω0_volume(prob)))
+    end
+    absorption .*= 1 / 3
+
+    save(example_path * "/optical_absorption_$(M_tol).jld2", "Erange", Erange, "absorption", absorption)
 end
 
 # %% compute errors
 
-# load reference spectrum
-Erange_exciting = readdlm(path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC11.OUT")[19:end, 1]
-absorption_exciting = zeros(length(Erange_exciting), 3)
-absorption_exciting[:, 1] = readdlm(path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC11.OUT")[19:end, 3]
-absorption_exciting[:, 2] = readdlm(path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC22.OUT")[19:end, 3]
-absorption_exciting[:, 3] = readdlm(path * "/EPSILON/EPSILON_BSE-singlet-TDA-BAR_SCR-full_OC33.OUT")[19:end, 3];
-
-E0 = 0.
-
-# some parameters
-σ = 0.0055
-g = ω -> 1 / π * σ / (ω^2 + σ^2)
-Ha_to_eV = 27.205144510369827
-Erange = Erange_exciting ./ Ha_to_eV
+f = h5open(example_path * "/exciton_coefficients.h5")
+eigenvalue_reference = read(f["eigvec-singlet-TDA-BAR-full"]["0001"]["evals"])[1]
+close(f)
+Erange, absorption_reference = load(example_path * "/optical_absorption_reference.jld2", "Erange", "absorption")
 
 errors_optical_absorption = []
 errors_ground_state_energy = []
 for M_tol in M_tol_vec
     # save results
-    α, β, norm_d2 = load(path * "/optical_absorption_lanczos_$(M_tol).jld2", "alpha", "beta", "norm d^2")
+    absorption = load(example_path * "/optical_absorption_$(M_tol).jld2", "absorption")
 
-    optical_absorption_lanc = norm_d2 * 8 * pi^2 / (prod(prob.N_ks) * prob.Ω0_vol) * BSE_k_ISDF.lanczos_optical_absorption(α, β, N_iter, g, Erange)
-
-    error_optical_absorption = norm(optical_absorption_lanc - absorption_exciting[:, direction], 1) * Ha_to_eV / length(optical_absorption_lanc)
+    error_optical_absorption = norm(absorption - absorption_reference[:, 1], 1) * (Erange[2] - Erange[1])
     push!(errors_optical_absorption, error_optical_absorption)
 
-    # eigenvalues, eigenvectors = eigs(H, which=:SR, nev = 1, maxiter=1000)
+    eigenvalues = load(example_path * "/eigs_$(M_tol).jld2", "eigenvalues")
 
+    push!(errors_ground_state_energy, abs(eigenvalues[1] - eigenvalue_reference))
 end
 
-save(path * "/errors_spectrum.jld2", "M_tol_vec", M_tol_vec,  "errors_optical_absorption", errors_optical_absorption)
+save(example_path * "/errors_spectrum.jld2", "M_tol_vec", M_tol_vec,  "errors_optical_absorption", errors_optical_absorption, "errors_ground_state_energy", errors_ground_state_energy)
