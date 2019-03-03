0d9de8e28db0d9a284c78451be92021ec34a6640
Felix Henneke <felix.henneke@fu-berlin.de>
2019-03-03 14:34:20 +0100

    changed banchmarks to use larger tolerance

--- experiments/benchmarks_3d.jl
+++ experiments/benchmarks_3d.jl
@@ -24,9 +24,12 @@ N_core = 0
 N_v = 4
 N_c = 5
 
-N_μ_vvs = ((3, 3, 3), 53)
-N_μ_ccs = ((4, 4, 4), 66)
-N_μ_vcs = ((3, 3, 3), 73)
+# N_μ_vvs = ((3, 3, 3), 53)
+# N_μ_ccs = ((4, 4, 4), 66)
+# N_μ_vcs = ((3, 3, 3), 73)
+N_μ_vvs = ((2, 2, 2), 42)
+N_μ_ccs = ((3, 3, 3), 93)
+N_μ_vcs = ((3, 3, 3), 53)
 
 #variable parameters
 N_ks_vec = [(2, 2, 2), (3, 3, 3), (4, 4, 4), (5, 5, 5), (7, 7, 7), (9, 9, 9), (13, 13, 13)]
@@ -52,15 +55,15 @@ for N_ks in N_ks_vec
     end
     t_H_x = @benchmark $h()
 
-    save(example_path * "$(N_ks...)_$(N_1d)/benchmark.jld2", "t_isdf", t_isdf, "t_H_setup", t_H_setup, "t_H_x", t_H_x)
+    save(example_path * "$(N_ks...)_$(N_1d)/benchmark_$(N_μ_vvs)_$(N_μ_ccs)_$(N_μ_vcs).jld2", "t_isdf", t_isdf, "t_H_setup", t_H_setup, "t_H_x", t_H_x)
 end
 
 # %% save results
 
-results = [load("diamond/$(N_ks...)_$(N_1d)/benchmark.jld2") for N_ks in N_ks_vec]
+results = [load("diamond/$(N_ks...)_$(N_1d)/benchmark_$(N_μ_vvs)_$(N_μ_ccs)_$(N_μ_vcs).jld2") for N_ks in N_ks_vec]
 timings = ["t_isdf", "t_H_setup", "t_H_x"]
 
 setup_times = sum(1e-9 .* time.(minimum.([res[t] for res in results, t in timings[1:2]])); dims = 2)
 evaluation_times = 1e-9 .* time.(minimum.([res[t] for res in results, t in timings[3:3]]))
 
-save(example_path * "/benchmark.jld2", "N_ks_vec", N_ks_vec, "setup_times", setup_times, "evaluation_times", evaluation_times)
+save(example_path * "/benchmark_$(N_μ_vvs)_$(N_μ_ccs)_$(N_μ_vcs).jld2", "N_ks_vec", N_ks_vec, "setup_times", setup_times, "evaluation_times", evaluation_times)
